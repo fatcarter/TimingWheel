530686f3e98779398fbdc8a31fd3ee3a87ae90d7
michaelzhang055 <michaelzhang055@hotmail.com>
2022-07-01 15:53:58 +0800

    feat: complete comment

--- src/main/java/cn/fatcarter/wheel/Bucket.java
+++ src/main/java/cn/fatcarter/wheel/Bucket.java
@@ -4,14 +4,36 @@ import java.util.List;
 import java.util.concurrent.Delayed;
 import java.util.function.Consumer;
 
+/**
+ * Timing Wheel slot
+ */
 public interface Bucket extends Delayed {
+    /**
+     * Add an entry to entry list
+     *
+     * @param entry the delayed entry object
+     */
     void addEntry(TimingEntry entry);
 
+    /**
+     * Set expiration time(milliseconds) for this bucket , return true if it was set for the first time , false otherwise
+     *
+     * @param expire The bucket timeout timestamp
+     */
     boolean setExpiration(long expire);
 
+    /**
+     * Get the bucket expiration timestamp
+     *
+     * @return
+     */
     long getExpiration();
+
 
     List<TimingEntry> entries();
 
+    /**
+     * Flush the bucket, and call consumer for all elements in the entry list
+     */
     void flush(Consumer<TimingEntry> consumer);
 }
--- src/main/java/cn/fatcarter/wheel/Propeller.java
+++ src/main/java/cn/fatcarter/wheel/Propeller.java
@@ -1,5 +1,8 @@
 package cn.fatcarter.wheel;
 
+/**
+ * TimingWheel propeller, used to propel the timing wheel forward
+ */
 public interface Propeller {
     void start();
 
